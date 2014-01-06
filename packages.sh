c4739f6ea90a42d27f5d13d52c53a479d73e71fe
William E. T. III <w3ttr3y@w3ttr3y.com>
2014-01-06 18:31:50 -0500

    Basic cleanup

--- packages.sh
+++ packages.sh
@@ -31,21 +31,6 @@ elif [ $packager = 'dpkg' -a $updater = 'apt' ] ; then
     package_list_dpkg_apt
 fi
 
-
-#
-#list_updates="yum check-update"
-# list_security="yum list-security"
-  #list="packages=\$(/bin/mktemp); updates=\$(/bin/mktemp); $list_package | sort > \$packages && $list_updates | sort >  \$updates && join -a 1 \$packages \$updates; rm \$packages; rm \$updates"
-  
-#elif [ $(command -v apt-get) ] ; then
-  #apt-get -s --just-print upgrade | perl -nle 'printf "%-30s %-5s %-25s %-25s %-11s\n",$1,$5,$2,$3,$4  if(/^Inst\s(\S+)\s\[([^]]+)]\s\((\S+)\s(\S+)(?:\s\[([^]]+)\])?/)'
-# updater="apt"
-# list_packages=""
-# list_updates=""
-# list_security=""
-#fi
-
-
 echo "Package list: $list_packages"
 echo "Updater list: $list_updates"
 echo "Security list: $list_security"
@@ -58,6 +43,3 @@ echo
 $list
 #$list_packages
 #$list_updates
-
-#eval $list
-#echo $($list)
