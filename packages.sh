e8a78b022add73c3fb205d490895006fa88c750a
William E. T. III <w3ttr3y@w3ttr3y.com>
2014-01-06 19:23:13 -0500

    Cleaned up script argument parsing and expanded help

--- packages.sh
+++ packages.sh
@@ -9,16 +9,38 @@ LANG="en_US.UTF-8"
 
 usage() {
   me=$(basename $0)
-  echo "$me"
+  echo "$me -- list package information
+
+    $me -[hpus]
+
+Lists information about installed packages.  The default is to display the complete information in a compact form (Name, Arc, Version, Vendor, Group, Update Version, Repo). Note: currently it excludes securityinformation
+
+    -h  Display this help information
+    -p  Display installed packages only (Name, arch, version, vendor, group)
+    -u  Display available updates only (name, update version, repo)
+    -s  Display available security updates only (Not Implemented; TODO)
+"
 }
 
+actions_list_package=0
+actions_list_updates=0
+actions_list_security=0
+actions_list_compact=0
+
 OPTIND=1
-while getopts "h" opt; do
+while getopts "hpus" opt; do
   case "$opt" in
     h) usage; exit;;
+    p) actions_list_package=1; actions_list_updates=0; actions_list_security=0; echo "packages";;
+    u) actions_list_package=0; actions_list_updates=1; actions_list_security=0;;
+    s) actions_list_package=0; actions_list_updates=0; actions_list_security=1;;
   esac
 done
 shift $((OPTIND-1))
+
+if [ $actions_list_package -eq 0 -a $actions_list_updates -eq 0 -a $actions_list_security -eq 0 ] ; then
+  actions_list_compact=1
+fi
 
 . `dirname $0`/common.sh
 
@@ -31,15 +53,12 @@ elif [ $packager = 'dpkg' -a $updater = 'apt' ] ; then
     package_list_dpkg_apt
 fi
 
-echo "Package list: $list_packages"
-echo "Updater list: $list_updates"
-echo "Security list: $list_security"
-echo "List: $list"
-echo
-echo
-
-
-
-$list
-#$list_packages
-#$list_updates
+if [ $actions_list_compact -eq 1 ] ; then
+    $list
+elif [ $actions_list_package -eq 1 ] ; then
+    $list_packages
+elif [ $actions_list_updates -eq 1 ] ; then
+    $list_updates
+elif [ $actions_list_security -eq 1 ] ; then
+    $list_security
+fi
