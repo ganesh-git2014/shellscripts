5118810222cfd3bcd260bea4b104b6fce49592e6
William E. T. III <w3ttr3y@w3ttr3y.com>
2014-01-07 20:49:30 -0500

    [Cleanup] Cleaned up the rpm implementation to be slightly more efficient when not used.

--- packages.d/rpm.sh
+++ packages.d/rpm.sh
@@ -8,19 +8,20 @@
 
 if [ -z "$PACKAGER" -a -n "$(command -v rpm )" ] ; then
     PACKAGER='rpm'
+
+    package_check_rpm() {
+        rpm --quiet -q $1 >/dev/null
+
+        if [ $? -eq 0 ] ; then
+            echo 0
+        else
+            echo 1
+        fi
+    }
+
+    package_list_rpm() {
+        rpm -qa --queryformat "%{NAME}.%{ARCH}\t%{NAME}\t%{ARCH}\t%{VERSION}-%{RELEASE}\t%{VENDOR}\t%{GROUP}\n" | grep -v 'gpg-pubkey.(none)'
+    }
 fi
 
-package_check_rpm() {
-    rpm --quiet -q $1 >/dev/null
-
-    if [ $? -eq 0 ] ; then
-        echo 0
-    else
-        echo 1
-    fi
-}
-
-package_list_rpm() {
-    rpm -qa --queryformat "%{NAME}.%{ARCH}\t%{NAME}\t%{ARCH}\t%{VERSION}-%{RELEASE}\t%{VENDOR}\t%{GROUP}\n" | grep -v 'gpg-pubkey.(none)'
-}
 
